03762282a8860c3262b331a37670b0d6fb75f12a
Lalit Rajak <lalit92rajak@gmail.com>
2022-07-12 04:37:39 +0530

    task3

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 //const http  =  require('http');
 const express =  require('express')
+const bodyParser =  require('body-parser')
+
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log('in the middleware');
-    next();
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use('/add-product',(req,res,next)=>{
+    //console.log('in the another middleware');
+    //res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit"> Add Product</button></form>');
+    res.send('<form action="/product" method="POST"><input type="number" name="size"><button type="submit"> Add Product</button></form>');
+
+});
+
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
 
 })
-app.use((req,res,next)=>{
-    console.log('in second middleware');
-    res.send('<h1>hello to node js</h1>')
-    //res.send( { key1: value })
+app.use('/',(req,res,next)=>{
+    //console.log('in second middleware');
+    res.send('<h1>Hello To Node js</h1>')
+   
 
 
 })
