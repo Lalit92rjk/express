e1a6d88b74fb8659d08716219d10d931e5aab4a1
Lalit Rajak <lalit92rajak@gmail.com>
2022-07-13 04:20:14 +0530

    task3 updated

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
-const express =  require('express');
-const bodyParser =  require('body-parser');
+//const http  =  require('http');
+const express =  require('express')
+const bodyParser =  require('body-parser')
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes =  require('./routes/shop');
+app.use(bodyParser.urlencoded({extended:false}))
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use('/add-product',(req,res,next)=>{
+    //console.log('in the another middleware');
+    //res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit"> Add Product</button></form>');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit"> Add Product</button></form>');
 
-app.use('/admin',adminRoutes);
-app.use('/shop',shopRoutes);
+});
 
-app.use((req,res,next)=>{
-    res.status(404).send('<h1>Page not found</h1>')
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+
+})
+app.use('/',(req,res,next)=>{
+    //console.log('in second middleware');
+    res.send('<h1>Hello To Node js</h1>')
+   
+
+
 })
 
-
-
-app.listen(3000);
+/*const server = http.createServer(app);
+server.listen(3000)*/
+app.listen(3000)
