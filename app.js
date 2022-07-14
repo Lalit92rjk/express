dbb22f68d386b09b15e2603bb55bbe5810ca192d
Lalit Rajak <lalit92rajak@gmail.com>
2022-07-15 02:13:58 +0530

    task5

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
-const express =  require('express');
-const bodyParser =  require('body-parser');
+const express=require('express');
 
 const app = express();
+const bodyParser = require('body-parser');
+const loginRoute=require('./group/login')
+const messageRoute = require('./group/message')
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes =  require('./routes/shop');
+app.use(bodyParser.urlencoded({ extended:false}))
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(loginRoute)
+app.use(messageRoute)
+app.listen(3000)
 
-app.use('/admin',adminRoutes);
-app.use('/shop',shopRoutes);
-
-app.use((req,res,next)=>{
-    res.status(404).send('<h1>Page not found</h1>')
-})
-
-
-
-app.listen(3000);
