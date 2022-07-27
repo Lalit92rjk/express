3991d7a1fe7958cf4f916527bc3ca1a7aa660081
Lalit Rajak <lalit92rajak@gmail.com>
2022-07-27 08:23:13 +0530

    task20

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 const sequelize  =  require('./util/database');
 const Product =  require('./models/product')
-const User =  require('./models/user')
+const User =  require('./models/user');
+const Cart =  require('./models/cart');
+const CartItem =  require('./models/cart-item');
 
 const app = express();
 
@@ -37,6 +39,10 @@ app.use(errorController.get404);
 
 Product.belongsTo(User,{ constraints: true ,onDelete: 'CASCADE'});
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product,{through: CartItem});
+Product.belongsToMany(Cart,{through: CartItem});
 
 sequelize
 //.sync({force: true})
@@ -54,9 +60,13 @@ sequelize
 })
 .then(user=> {
     //console.log(user)
-    app.listen(3000)
+    return user.createCart()
+   
 })
+.then(cart=>{
+    app.listen(3000);
 
+})
 .catch(err=>{
     console.log(err);
 })
